09bdd28e025cbf60b619c769a0f3e418e6e0c65d
Christian Haney <christianhaney1996@gmail.com>
2019-11-14 12:19:56 -0800

    SGT | React - Christian Haney (functionality) added constructor and initial state to App

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { grades: [], error: '' };
+  }
   render() {
     return null;
   }
