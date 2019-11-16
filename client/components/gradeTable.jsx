74b9c16b93ca299654adce4cea3b9bf3f561e2fb
Christian Haney <christianhaney1996@gmail.com>
2019-11-15 16:32:42 -0800

    SGT | React - Christian Haney (functionality) modified render method of GradeTable component to pass the callback to deleteGrade down to the Grade component

--- client/components/gradeTable.jsx
+++ client/components/gradeTable.jsx
@@ -3,7 +3,7 @@ import Grade from './grade';
 
 function GradeTable(props) {
   let grades = props.grades.map(grade => (
-    <Grade key ={grade.id} name={grade.name} course={grade.course} grade={grade.grade}/>
+    <Grade key ={grade.id} onDelete={props.onDelete} name={grade.name} course={grade.course} grade={grade.grade}/>
   ));
   return (
     <table className='table table-striped table-bordered order-2 order-md-1 col-xs-12 col-md-9'>
