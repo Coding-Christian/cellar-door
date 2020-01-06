a8713692194fda86f6ebcc0dee69bdaa1dbd0a26
Coding-Christian <christianhaney1996@gmail.com>
2020-01-06 14:07:35 -0800

    modified Grade component to be stateful state tracks grade info and whether or not editing is in progress conditionally render second set of buttons for edit controls

--- client/components/grade.jsx
+++ client/components/grade.jsx
@@ -1,16 +1,56 @@
 import React from 'react';
 
-function Grade(props) {
-  return (
-    <tr>
-      <td>{props.name}</td>
-      <td>{props.course}</td>
-      <td>{props.grade}</td>
-      <td>
-        <button onClick={() => props.onDelete(props.id)} className='btn btn-danger'>Delete</button>
-      </td>
-    </tr>
-  );
+class Grade extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+      info: {
+        name: props.name,
+        course: props.course,
+        grade: props.grade
+      }
+    };
+    this.id = props.id;
+    this.initialInfo = { ...this.state.info };
+    this.onDelete = props.onDelete;
+    this.resetInfo = this.resetInfo.bind(this);
+  }
+  resetInfo() {
+    this.setState({ editing: false, info: this.initialInfo });
+  }
+  render() {
+    let infoElems, btnElems;
+    if (this.state.editing) {
+      btnElems = [
+        <button onClick={() => this.setState({ editing: false }) } key='confirm' className='btn btn-success mr-1'>Confirm</button>,
+        <button onClick={this.resetInfo} key='cancel' className='btn btn-secondary mr-1'>Cancel</button>
+      ];
+      infoElems = [
+        <td key='name'>{this.state.info.name}</td>,
+        <td key='course'>{this.state.info.course}</td>,
+        <td key='grade'>{this.state.info.grade}</td>
+      ];
+    } else {
+      btnElems = [
+        <button onClick={() => this.setState({ editing: true }) } key='edit' className='btn btn-primary mr-1'>Edit</button>,
+        <button onClick={() => this.onDelete(this.id)} key='delete' className='btn btn-danger'>X</button>
+      ];
+      infoElems = [
+        <td key='name'>{this.state.info.name}</td>,
+        <td key='course'>{this.state.info.course}</td>,
+        <td key='grade'>{this.state.info.grade}</td>
+      ];
+    }
+    return (
+      <tr>
+        {infoElems}
+        <td className='d-flex justify-content-end'>
+          {btnElems}
+        </td>
+      </tr>
+    );
+  }
 }
 
 export default Grade;
