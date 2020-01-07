62fc0abb7fc127e1f3df84fd24a6e2b8a54677da
Coding-Christian <christianhaney1996@gmail.com>
2020-01-06 16:39:28 -0800

    inputs made during edit of a grade are now validated error is displayed if an invalid input is submitted

--- client/components/grade.jsx
+++ client/components/grade.jsx
@@ -9,13 +9,15 @@ class Grade extends React.Component {
         name: props.name,
         course: props.course,
         grade: props.grade
-      }
+      },
+      error: ''
     };
     this.id = props.id;
     this.initialInfo = { ...this.state.info };
     this.onDelete = props.onDelete;
     this.resetInfo = this.resetInfo.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   resetInfo() {
     this.setState({ editing: false, info: this.initialInfo });
@@ -25,11 +27,28 @@ class Grade extends React.Component {
     info[event.target.id] = event.target.value;
     this.setState({ info });
   }
+  handleSubmit() {
+    const wordPatt = /\w*[!@#$%^&*()]+\w*/g;
+    const { name, course } = this.state.info;
+    const grade = Number(this.state.info.grade);
+    if (name.match(wordPatt) || name.length < 2 || name.length > 60) {
+      this.setState({ error: 'Invalid Name' });
+    } else if (course.match(wordPatt) || course.length < 2 || course.length > 60) {
+      this.setState({ error: 'Invalid Course' });
+    } else if (isNaN(grade) || grade < 0 || grade > 200) {
+      this.setState({ error: 'Invalid Grade' });
+    } else {
+      this.setState({ editing: false, error: '' });
+    }
+  }
   render() {
-    let infoElems, btnElems;
+    let infoElems, btnElems, errorClass;
+    if (this.state.error === '') {
+      errorClass = 'd-none';
+    } else errorClass = '';
     if (this.state.editing) {
       btnElems = [
-        <button onClick={() => this.setState({ editing: false }) } key='confirm' className='btn btn-success mr-1'>Confirm</button>,
+        <button onClick={ this.handleSubmit } key='confirm' className='btn btn-success mr-1'>Confirm</button>,
         <button onClick={this.resetInfo} key='cancel' className='btn btn-secondary mr-1'>Cancel</button>
       ];
       infoElems = [
@@ -57,8 +76,11 @@ class Grade extends React.Component {
     return (
       <tr>
         {infoElems}
-        <td className='d-flex justify-content-end'>
-          {btnElems}
+        <td className='d-flex flex-wrap justify-content-end'>
+          <div className={'alert alert-danger w-100 ' + errorClass}>{this.state.error}</div>
+          <div>
+            {btnElems}
+          </div>
         </td>
       </tr>
     );
