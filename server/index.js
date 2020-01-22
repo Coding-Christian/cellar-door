c176d93a263dc88731e24251a82f7266f23d7681
Christian Haney <christianhaney1996@gmail.com>
2020-01-22 11:54:32 -0800

    can delete location on backend if empty

--- server/index.js
+++ server/index.js
@@ -137,6 +137,28 @@ server.delete('/api/groceries/:id', async (req, res) => {
   }
 });
 
+server.delete('/api/locations/:id', async (req, res) => {
+  if (!req.params.id) {
+    res.status(400).send('Student ID required');
+  } else {
+    const sql = `SELECT id FROM groceryItems WHERE locationId = ${mysql.escape(req.params.id)}`;
+    const results = await makeQuery(sql)
+      .catch(() => { res.status(500).send('An Error occurred while connecting to the database'); });
+    if (results.length) {
+      res.status(409).send(`Location with ID ${req.params.id} is not empty.`);
+    } else {
+      const sql = `DELETE FROM storageLocations WHERE id = ${mysql.escape(req.params.id)}`;
+      const results = await makeQuery(sql)
+        .catch(() => { res.status(500).send('An Error occurred while connecting to the database'); });
+      if (!results.affectedRows) {
+        res.status(404).send(`Location with ID ${req.params.id} not found`);
+      } else {
+        res.status(200).send({ 'id': Number(req.params.id) });
+      }
+    }
+  }
+});
+
 server.delete('/api/grades/:id', async (req, res) => {
   if (!req.params.id) {
     res.status(400).send('Student ID required');
