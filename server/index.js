93d2f58b707593ab7df851d5f3b6108f10718a50
Christian Haney <christianhaney1996@gmail.com>
2020-01-22 12:37:02 -0800

    locations can be updated on backend

--- server/index.js
+++ server/index.js
@@ -159,6 +159,27 @@ server.delete('/api/locations/:id', async (req, res) => {
   }
 });
 
+server.patch('/api/locations', async (req, res) => {
+  const reqProps = ['id', 'name', 'description'];
+  for (const prop in reqProps) {
+    if (!req.body[reqProps[prop]]) {
+      res.status(400).send(`Location ${reqProps[prop]} required.`);
+      return;
+    }
+  }
+  const { id, name, description } = req.body;
+  let sql = 'UPDATE storageLocations SET locationName = ?, description = ? WHERE id = ?';
+  const inserts = [name, description, id];
+  sql = mysql.format(sql, inserts);
+  const results = await makeQuery(sql)
+    .catch(() => { res.status(500).send('An Error occurred while connecting to the database'); });
+  if (!results.affectedRows) {
+    res.status(404).send(`Location with ID ${req.params.id} not found`);
+  } else {
+    res.status(200).send(req.body);
+  }
+});
+
 server.delete('/api/grades/:id', async (req, res) => {
   if (!req.params.id) {
     res.status(400).send('Student ID required');
