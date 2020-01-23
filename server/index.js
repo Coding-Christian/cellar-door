4f39863e351a95f9b3387cda72aec5185cb32ed9
Christian Haney <christianhaney1996@gmail.com>
2020-01-22 16:21:22 -0800

    can add location on backend

--- server/index.js
+++ server/index.js
@@ -206,6 +206,23 @@ server.patch('/api/locations', async (req, res) => {
   }
 });
 
+server.post('/api/locations', async (req, res) => {
+  const reqProps = ['name', 'description'];
+  for (const prop in reqProps) {
+    if (!req.body[reqProps[prop]]) {
+      res.status(400).send(`Location ${reqProps[prop]} required.`);
+      return;
+    }
+  }
+  const { name, description } = req.body;
+  let sql = 'INSERT INTO storageLocations (id, locationName, description) VALUES (NULL, ?, ?)';
+  const inserts = [name, description];
+  sql = mysql.format(sql, inserts);
+  const results = await makeQuery(sql)
+    .catch(() => { res.status(500).send('An Error occurred while connecting to the database'); });
+  res.status(200).send({ 'id': results.insertId, 'name': name, 'description': description });
+});
+
 server.delete('/api/grades/:id', async (req, res) => {
   if (!req.params.id) {
     res.status(400).send('Student ID required');
