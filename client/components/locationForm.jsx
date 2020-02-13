07996343b3262287fec3e579a5f5979f8744ae71
Christian Haney <christianhaney1996@gmail.com>
2020-02-13 13:48:55 -0800

    added LocationForm methods to validate, submit and clear form

--- client/components/locationForm.jsx
+++ client/components/locationForm.jsx
@@ -15,6 +15,39 @@ class LocationForm extends React.Component {
         value: ''
       }
     };
+    this.onAdd = props.onAdd;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+  }
+  validateForm() {
+    const wordPatt = /[^\w\s]/g;
+    let name = Object.assign(this.state.name);
+    name.isValid = !(wordPatt.test(name.value) || name.value.length < 2 || name.value.length > 60);
+    this.setState({ name });
+  }
+  async handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.name.isValid) {
+      const status = await this.onAdd({
+        name: this.state.name.value,
+        description: this.state.description.value
+      });
+      if (status < 300) {
+        this.handleClear();
+        this.setState({ advancedView: false });
+      } else {
+        this.setState({ error: 'Could not reach server. Please try again.' });
+      }
+    }
+  }
+  handleClear() {
+    const newState = {
+      name: Object.assign(this.state.name),
+      description: { title: 'Description', value: '' }
+    };
+    newState.name.value = '';
+    newState.name.isValid = false;
+    this.setState(newState);
   }
   render() {
     return null;
